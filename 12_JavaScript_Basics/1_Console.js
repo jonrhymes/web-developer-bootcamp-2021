f9bd666b01264d8995f469d0714dac0b22bc7156
jonrhymes <jonrhymes@gmail.com>
2021-10-28 10:55:12 -0400

    JS variables

---
+++ 12_JavaScript_Basics/1_Console.js
@@ -0,0 +1,13 @@
+// REPL: READ, EVALUATE, PRINT, LOOP
+// Use DevTools to quickly test
+
+// Two asterisks put a number to the nth power.
+
+console.log(2 ** 4);
+
+// Javascript obeys order of operations (PEMDAS)
+// Parentheses, Exponents, Multiplication, Division, Addition, Subtraction
+
+
+// NaN is a 'number'
+console.log(typeof NaN);
---
+++ 12_JavaScript_Basics/2_Variables.js
@@ -0,0 +1,32 @@
+// jars that store values
+// let someName = value;
+
+
+let numHens = 5;
+let numRoosters = 1;
+
+console.log(numHens + numRoosters);
+
+// snapshot in time. If numHens is updated later, totalChickens's value will not change
+// Not intrinsically linked
+let totalChickens = numHens + numRoosters;
+
+// Define a variable called myLuckyNumber and set it equal to 37
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+
+// updating variables
+let numLives = 9;
+
+// increment
+numLives++;
+numLives += 4;
+numLives = numLives + 1;
+
+// decrement
+numLives--;
+numLives -= 3;
+numLives = numLives - 2;
+
+console.log(numLives);
