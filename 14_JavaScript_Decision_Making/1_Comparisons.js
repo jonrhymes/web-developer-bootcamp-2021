ea0afb05276d9a67316d4d0afec20ec03651a15b
jonrhymes <jonrhymes@gmail.com>
2021-10-28 16:50:59 -0400

    comparisons

---
+++ 14_JavaScript_Decision_Making/1_Comparisons.js
@@ -0,0 +1,29 @@
+// boolean logic
+// if a condition is met, do something
+// if not, don't do anything
+
+
+// comparisons
+
+3 > 2 // greater than
+2 < 3 // less than
+3 >= 2 // greater than or equal to
+2 <= 3 // less than or equal to
+2 == 2 // equality
+2 != 3 // not equal
+3 === 3 // strict equality
+2 !== 3 // strict non-equality
+
+
+// examples
+1 > 3 // false
+1 < 3 // true
+-1 > -1 // false
+
+// less than-, greater than- or equal to
+-1 <= -1 // true
+-1 <= 10 // true
+
+
+// underlying unicode compared
+'A' < 'a' // true, 0041 < 0061
