f7131d9ef54351d364662bded3f8e839e652581b
jonrhymes <jonrhymes@gmail.com>
2022-09-05 09:41:14 -0400

    Todos list and hackerrank

---
+++ 18_Functions/2_First_Function/app.js
@@ -0,0 +1,19 @@
+// 2 step process:
+// 1. Define the function
+// 2. Run it, execute it
+
+// keyword function
+// functions are camelCase
+function singSong() {
+    console.log("DO");
+    console.log("RE");
+    console.log("MI");
+}
+
+singSong();
+
+function printHeart() {
+    console.log("<3");
+}
+
+printHeart();
