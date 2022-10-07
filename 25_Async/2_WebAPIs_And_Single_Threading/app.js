8e2875260d71db2b784b422a28a04b6e5e3953e6
jonrhymes <jonrhymes@gmail.com>
2022-10-06 21:30:46 -0400

    web api

---
+++ 25_Async/2_WebAPIs_And_Single_Threading/app.js
@@ -0,0 +1,11 @@
+// single threaded
+// JavaScript runs at most one line of code
+// takes time - will app grind to a halt
+
+console.log("Sending request to server!"); // runs 1st
+setTimeout(() => {
+    console.log("Here is your data from the server...")
+}, 3000); // runs 3rd
+console.log("I AM AT THE END OF THE FILE!") // runs 2nd
+
+// browser does the work - uses Web APIs that handle tasks in the background
